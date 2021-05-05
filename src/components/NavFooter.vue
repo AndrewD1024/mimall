611e95590cd19d5e8adbff882544628a28cdc78c
dx <dx2015dx@outlook.com>
2021-05-06 00:09:07 +0800

    项目目录结构设置

---
+++ src/components/NavFooter.vue
@@ -0,0 +1,11 @@
+// 首页底部导航栏组件
+
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "nav-header", // 组件名称
+};
+</script>
---
+++ src/components/NavHeader.vue
@@ -0,0 +1,11 @@
+// 首页导航栏组件
+
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "nav-header", // 组件名称
+};
+</script>
