531d67bad9d8fff94be1647388db6649a7837604
dx <dx2015dx@outlook.com>
2021-05-06 17:53:06 +0800

    Mock设置

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,21 @@
 export default {
   name: "App",
   components: {},
+  data() {
+    return {
+      res: {},
+    };
+  },
+  mounted() {
+    // Mock模拟数据方式一：本地加载请求静态json文件的形式
+    // this.axios.get("/mock/user/login.json").then((res) => {
+    //   this.res = res;
+    // });
+    // Mock模拟数据方式三：本地继承mockjs实现数据mock
+    this.axios.get("/user/login.json").then((res) => {
+      this.res = res;
+    });
+  },
 };
 </script>
 
